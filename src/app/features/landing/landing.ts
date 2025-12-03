ae157064e7e57acacc944b31e1c21866852dd439
10GO <mixelidzet@gmail.com>
2025-12-03 23:42:23 +0400

    landing

--- src/app/features/landing/landing.ts
+++ src/app/features/landing/landing.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Header} from '../../shared/header/header';
 import {Footer} from '../../shared/footer/footer';
 
